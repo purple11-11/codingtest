212c3d621db9c33934dd0a0b8e43f2a96eee1315
young <125553827+yyi9546@users.noreply.github.com>
2023-06-09 23:17:42 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "         ,r'\"7";
+		String b = "r`-_   ,'  ,/";
+		String c = " \\. \". L_r'";
+		String d = "   `~\\/";
+		String e = "      |";
+		String f = "      |";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+
+}
