850ee734a5836c21142401de08ecc68b26cd2f54
young <125553827+yyi9546@users.noreply.github.com>
2023-06-16 01:21:32 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String[] args) {
+	
+		String id;
+		Scanner sc = new Scanner(System.in);
+		id = sc.next();
+		
+		System.out.println(id+"??!");
+		sc.close();
+	}
+	
+}
