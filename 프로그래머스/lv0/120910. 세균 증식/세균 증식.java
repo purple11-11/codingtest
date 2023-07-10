a4f002f03fe0acfaa2b65c5b78ae74e7a8ae280e
young <purple11_11@naver.com>
2023-07-10 22:00:39 +0900

    [level 0] Title: 세균 증식, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        for(int i = 1; i <= t; i++) {
+            answer = n * (int)Math.pow(2,t);
+        }
+        return answer;
+    }
+}
