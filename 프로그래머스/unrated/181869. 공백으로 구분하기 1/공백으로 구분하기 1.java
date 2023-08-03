2c2af1d4370eaa4d163b8461e24af21e996ccb3d
young <purple11_11@naver.com>
2023-08-03 10:13:06 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = new String[my_string.length()];
+        answer = my_string.split(" ");
+        return answer;
+    }
+}
