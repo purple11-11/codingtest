9d54ad6573051ad6f93fda866ae5a3c7c81330b0
young <purple11_11@naver.com>
2023-10-18 21:53:12 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,9 @@
+const solution = (my_string, is_prefix) => {
+    for(let i = 0; i < is_prefix.length; i++) {
+        if(my_string[i] !== is_prefix[i]) {
+            return 0;
+        }
+    
+    }
+    return 1;
+}
