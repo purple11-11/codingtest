653ef3da4e806d0b0e783a49e6c86dd8e87f2a17
young <purple11_11@naver.com>
2023-11-06 00:57:42 +0900

    [level unrated] Title: 문자열 붙여서 출력하기, Time: 46.54 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+    
+    input = line.split(' ');
+    
+}).on('close', () => {
+    let str1 = input[0];
+    let str2 = input[1];
+    console.log(str1 + str2);
+});
