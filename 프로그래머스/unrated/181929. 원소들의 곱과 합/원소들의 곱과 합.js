37dbf4979173f9d26ff14090bd3996122436c6da
young <purple11_11@naver.com>
2023-11-06 00:42:55 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -2,10 +2,10 @@ const solution = (num_list) => {
    
     let mul = num_list.reduce((acc, cur) => {
         return acc * cur;
-    });
+    }, 1);
     let sum = num_list.reduce((acc, cur) => {
         return acc + cur;
-    })
+    }, 0);
     
     return (mul < (sum*sum)) ? 1 : 0;
 }
