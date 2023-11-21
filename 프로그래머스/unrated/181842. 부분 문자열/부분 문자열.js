3445f95154c66f2829ae06fe18064ac092fa817e
young <purple11_11@naver.com>
2023-11-21 17:26:35 +0900

    [level unrated] Title: 부분 문자열, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
@@ -1,6 +1,1 @@
-const solution = (str1, str2) => {    
-    if(str2.includes(str1)) {
-        return 1;
-    } else
-    return 0;
-}
+const solution = (str1, str2) => (str2.includes(str1)) ? 1: 0;
