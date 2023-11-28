07b5578e83f103d928f1a9ba1a5950128a81d4a5
young <purple11_11@naver.com>
2023-11-28 16:19:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,12 +1,3 @@
-function solution(n)
-{
-    let num = String(n).split('');
-    let answer = num.reduce((acc,cur) => {
-        return acc + (cur*1);
-    },0)
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
+const solution = (n) => {
+    return String(n).split('').reduce((acc,cur) => acc + (cur*1),0);
 }
