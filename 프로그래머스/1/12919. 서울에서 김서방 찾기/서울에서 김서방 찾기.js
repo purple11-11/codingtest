1e766c9528a5185f05518a6b6d853a24487e323c
young <purple11_11@naver.com>
2023-12-02 18:03:50 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,4 @@
 function solution(seoul) {
-    let answer = '';
-    for(let i = 0; i < seoul.length; i++) {
-        if (seoul[i].includes("Kim"))  {answer = `김서방은 ${i}에 있다`}
-    }
-    return answer;
+
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
