7c427801c974d4b034f8d9d1959009b58411ee8a
young <purple11_11@naver.com>
2023-12-07 14:51:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,4 @@
 function solution(phone_number) {
-    var answer = '';
-    for(let i = 0; i < phone_number.length; i++) {
-        if(i < phone_number.length - 4) answer += phone_number.replace(/^[0-9]*$/g, '*')
-        else answer += phone_number[i]
-    }
+    let answer = phone_number.replace(/\d{4}/g, '*');
     return answer;
 }
