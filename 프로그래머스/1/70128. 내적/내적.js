111dc6fb20031eb1f5e918e594635dabdfab55e1
young <purple11_11@naver.com>
2023-12-12 14:53:53 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,9 +1,13 @@
 function solution(a, b) {
-    let answer = 0;
-    let cal = [];
-    for(let i = 0; i < a.length; i++) {
-        cal.push(a[i] * b[i]);
-    }
+//     let cal = [];
+//     for(let i = 0; i < a.length; i++) {
+//         cal.push(a[i] * b[i]);
+//     }
     
-    return cal.reduce((acc, cur) => acc + cur, 0);
+//     return cal.reduce((acc, cur) => acc + cur, 0);
+    let sum = 0;
+    for(let i = 0; i < a.length; i++) {
+        sum += a[i] * b[i];
+    }
+    return sum;
 }
