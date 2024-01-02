024139fabeb7fdb0f1a9b4f28c4dfe785234600d
young <purple11_11@naver.com>
2024-01-02 17:41:52 +0900

    [level 1] Title: 예산, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,8 +1,8 @@
 function solution(d, budget) {
     let answer = 0;
-    d.sort((a,b) => a - b);
+    d.sort((a,b) => a - b); // 오름차순 정렬
     for(let i of d) {
-        if(budget < i) break;
+        if(budget < i) break; // 예산 초과시 종료
          budget -= i;
          answer++;       
     }
