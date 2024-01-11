357ce3de474526d8cd4355ad01e74224dcfe5848
young <purple11_11@naver.com>
2024-01-11 14:22:28 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,10 @@
 function solution(a, b) {
-    let answer = 0;
-    let sum1 = parseInt(String(a) + String(b));
-    let sum2 = parseInt(String(b) + String(a));
-    sum1 > sum2 ? answer = sum1 : answer = sum2;
-    return answer;
+    // 내 첫 풀이
+    // let answer = 0;
+    // let sum1 = parseInt(String(a) + String(b));
+    // let sum2 = parseInt(String(b) + String(a));
+    // sum1 > sum2 ? answer = sum1 : answer = sum2;
+    // return answer;
+    
+    return Math.max(parseInt(`${a}${b}`), parseInt(`${b}${a}`))
 }
