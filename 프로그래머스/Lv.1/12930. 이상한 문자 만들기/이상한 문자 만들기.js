488e59475773ebbe9b189e8a69c824880b50fc08
young <purple11_11@naver.com>
2024-01-12 21:48:34 +0900

    [level Lv.1] Title: 이상한 문자 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/Lv.1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -14,21 +14,17 @@
 function solution(s) {
     let answer = '';
     let words = s.split(" ");
-    
-    for (let i = 0; i < words.length; i++) {
+
+    for(let i = 0; i < words.length; i++) {
         let word = words[i];
-        for (let j = 0; j < word.length; j++) {
-            if (j % 2 === 0) {
+        for(let j = 0; j < word.length; j++) {
+            if(j%2 === 0) {
                 answer += word[j].toUpperCase();
             } else {
                 answer += word[j].toLowerCase();
             }
         }
-        
-        if (i !== words.length - 1) {
-            answer += " ";
-        }
+        if(i !== words.length - 1) answer += " ";
     }
-    
     return answer;
 }
