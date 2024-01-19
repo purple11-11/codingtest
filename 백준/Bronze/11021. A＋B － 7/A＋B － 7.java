bf48b7d42560907c2964b15e14fc0191992f2bff
young <purple11_11@naver.com>
2024-01-19 11:53:36 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        int[] B = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+            B[i] = sc.nextInt();
+        }
+        sc.close();
+        
+        for(int i = 0; i < N; i++) {
+            System.out.println("Case #"+(i+1)+": "+(A[i]+B[i]));
+        }
+    }
+}
