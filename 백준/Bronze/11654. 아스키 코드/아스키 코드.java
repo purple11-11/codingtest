fe211b16d7c45800f13a09d9be42e5e874345a2f
young <purple11_11@naver.com>
2024-01-28 16:15:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char N = sc.next().charAt(0);
+        sc.close();
+
+        System.out.println(N - 0);
+    }
+
+}
