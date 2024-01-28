2ea7639a33e079b88713af22916672ee0de955c1
young <purple11_11@naver.com>
2024-01-28 15:20:15 +0900

    [Bronze IV] Title: 기찍 N, Time: 1092 ms, Memory: 32824 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        
+        for(int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+        
+    }
+
+}
