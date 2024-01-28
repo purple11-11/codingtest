a7e9308c58d00983a42bbb788132b403c83b0a7b
young <purple11_11@naver.com>
2024-01-28 16:03:59 +0900

    [Bronze IV] Title: 숫자의 합, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -10,7 +10,10 @@ class Main {
 
         int sum = 0;
         for(int i = 0; i < N; i++) {
-            sum += A.charAt(i) - '0';
+            sum += A.charAt(i) - '0'; // String to int(문자열에서 각 문자를 숫자로 변환하여 합산)
+            // OR
+            // sum += A.charAt(i) - 48;
+            // 문자의 ASCII 코드에서 정수 값 48('0')을 빼면 해당 문자에 대응하는 숫자 값을 얻을 수 있다.
         }
 
         System.out.print(sum);
