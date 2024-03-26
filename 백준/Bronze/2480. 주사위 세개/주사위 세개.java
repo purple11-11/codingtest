36c0075b9c03f2b8403a162e971af04b38498143
young <purple11_11@naver.com>
2024-03-26 13:41:51 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+//        sc.nextLine(); // 개행 문자 제거
+        sc.close();
+
+        int result = 0;
+        int[] numArr = {num1, num2, num3};
+        int max = Arrays.stream(numArr).max().getAsInt();
+
+        if(num1 == num2 && num2 == num3) {
+            result = 10000 + (num1*1000);
+        } else if (num1 == num2 ){
+            result = 1000 + (num1*100);
+        } else if (num1 == num3 ){
+            result = 1000 + (num1*100);
+        }else if (num2 == num3 ){
+            result = 1000 + (num2*100);
+        } else {
+            result = max * 100;
+        }
+        System.out.println(result);
+    }
+}
