4103cc90e91825dee3f48115f227d214771e642f
young <purple11_11@naver.com>
2024-04-08 00:00:11 +0900

    [Bronze II] Title: 다이얼, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        sc.close();
+
+        String[] arr = N.split("");
+        int answer = 0;
+        for(int i = 0; i < arr.length; i++) {
+            char a = arr[i].charAt(0); // 문자로 직접 비교하기 위해 char형으로 변환
+            if('A' <= a && a <= 'C') answer += 3;
+            if('D' <= a && a <= 'F') answer += 4;
+            if('G' <= a && a <= 'I') answer += 5;
+            if('J' <= a && a <= 'L') answer += 6;
+            if('M' <= a && a <= 'O') answer += 7;
+            if('P' <= a && a <= 'S') answer += 8;
+            if('T' <= a && a <= 'V') answer += 9;
+            if('W' <= a && a <= 'Z') answer += 10;
+        }
+        System.out.println(answer);
+    }
+}
