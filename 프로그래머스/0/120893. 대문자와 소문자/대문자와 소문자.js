fe0b9f69b399e6fd0fb416fa0ff9f2c4ccab042e
young <purple11_11@naver.com>
2026-07-07 19:46:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.21 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,13 +1,3 @@
 function solution(my_string) {
-    let answer = '';
-    
-    for(let i = 0; i < my_string.length; i++) {
-     const char = my_string[i];
-        if(char === char.toUpperCase()){
-            answer += char.toLowerCase();
-        } else {
-            answer += char.toUpperCase();
-        }
-    }
-    return answer;
+   return my_string.split('').map(c => c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()).join('');
 }
