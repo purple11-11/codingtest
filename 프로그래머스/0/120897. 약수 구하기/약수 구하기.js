2fdf34799ba8c3fb6311a6da20a307834b615107
young <purple11_11@naver.com>
2026-07-07 19:53:56 +0900

    [level 0] Title: 약수 구하기, Time: 0.25 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,10 @@
 function solution(n) {
-// 1. % 0인 숫자 배열
-    const arr = [];
+    const answer = [];
     for(let i = 0; i <= n; i++) {
         if(n%i === 0) {
-            arr.push(i);
+            answer.push(i);
         }
     }
-    // 2. 오름차순 정렬
-    return arr.sort((a, b) => a - b);
+
+    return answer
 }
