5e549252e2dbead980fc7cfb13d98e6cb473c23b
young <purple11_11@naver.com>
2026-07-08 20:08:57 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.06 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,13 +1,4 @@
 function solution(array) {
-    let maxValue = array[0];
-    let maxIndex = 0;
-  
-    for(let i = 0; i < array.length; i++){
-        if(array[i] > maxValue) {
-            maxValue = array[i];
-            maxIndex = i;
-        }
-    }
-    
-    return [maxValue, maxIndex];
+    const maxValue = Math.max(...array)
+    return [maxValue,array.indexOf(maxValue)];
 }
